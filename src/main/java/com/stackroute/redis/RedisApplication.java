33a70bef51087d3f4e5068a2ee43d2ef586db888
Bharath <bharas21@in.ibm.com>
2019-02-11 17:55:30 +0530

    final Redis

---
+++ src/main/java/com/stackroute/redis/RedisApplication.java
@@ -0,0 +1,29 @@
+package com.stackroute.redis;
+
+import com.stackroute.redis.Model.Item;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+public class RedisApplication {
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory(){
+		return new JedisConnectionFactory();
+	}
+	@Bean
+	RedisTemplate<String, Item> redisTemplate(){
+		RedisTemplate<String,Item> redisTemplate = new RedisTemplate<String, Item>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+
+}
+
